1836dc31bc85b9fcb8017f50eef2289b1edc00c8
Alexandre Piel <alexandre.piel@zooplus.com>
2023-08-15 19:08:03 +0200

    wip generator send

--- src/main.ts
+++ src/main.ts
@@ -1,72 +1,14 @@
 import { createCanvas } from 'canvas';
 import { getSerial } from './serial';
+import { COMMANDS, HAPTIC } from './constants';
 
-const COMMANDS = {
-  BUTTON_PRESS: 0x00,
-  KNOB_ROTATE: 0x01,
-  SET_COLOR: 0x02,
-  SERIAL: 0x03,
-  RESET: 0x06,
-  VERSION: 0x07,
-  SET_BRIGHTNESS: 0x09,
-  FRAMEBUFF: 0x10,
-  SET_VIBRATION: 0x1b,
-  MCU: 0x0d,
-  DRAW: 0x0f,
-  TOUCH: 0x4d,
-  TOUCH_CT: 0x52,
-  TOUCH_END: 0x6d,
-  TOUCH_END_CT: 0x72,
-};
+
 
 const displays = {
   center: { id: Buffer.from('\x00A'), width: 360, height: 270 }, // "A"
   left: { id: Buffer.from('\x00L'), width: 60, height: 270 }, // "L"
   right: { id: Buffer.from('\x00R'), width: 60, height: 270 }, // "R"
 };
-
-let transactionID = 0;
-async function send(command: number, data = Buffer.alloc(0)) {
-  transactionID = (transactionID + 1) % 256;
-  // Skip transaction ID's of zero since the device seems to ignore them
-  if (transactionID === 0) transactionID++;
-  const header = Buffer.alloc(3);
-  header[0] = Math.min(3 + data.length, 0xff);
-  header[1] = command;
-  header[2] = transactionID;
-  const packet = Buffer.concat([header, data]);
-  const serial = await getSerial();
-
-  // Large messages
-  if (packet.length > 0xff) {
-    const prep = Buffer.alloc(14);
-    prep[0] = 0x82;
-    prep[1] = 0xff;
-    prep.writeUInt32BE(packet.length, 6);
-    serial.connection.write(prep);
-    // Small messages
-  } else {
-    // Prepend each message with a send indicating the length to come
-    const prep = Buffer.alloc(6);
-    prep[0] = 0x82;
-    prep[1] = 0x80 + packet.length;
-    serial.connection.write(prep);
-  }
-  serial.connection.write(packet);
-
-  // This is not good cause it will break the generator loop
-  const waitTransaction = (res: (value: any) => void) => {
-    serial.parser.once('data', (buff) => {
-      if (transactionID == buff[2]) {
-        console.log('transactionID matches');
-        res(void 0);
-      } else {
-        waitTransaction(res);
-      }
-    });
-  };
-  return new Promise(waitTransaction);
-}
 
 async function drawSomething() {
   const canvas = createCanvas(100, 200);
@@ -92,16 +34,21 @@ async function drawSomething() {
   header.writeUInt16BE(w, 4);
   header.writeUInt16BE(h, 6);
 
+  const serial = await getSerial();
+
   // Write to frame buffer
-  await send(COMMANDS.FRAMEBUFF, Buffer.concat([displays.center.id, header, buffer]));
+  await serial.send(COMMANDS.FRAMEBUFF, Buffer.concat([displays.center.id, header, buffer]));
 
   // Draw to display
-  await send(COMMANDS.DRAW, displays.center.id);
+  await serial.send(COMMANDS.DRAW, displays.center.id);
 }
 
 async function main() {
   const serial = await getSerial();
-  await drawSomething();
+
+  await serial.send(COMMANDS.SET_VIBRATION, Buffer.from([HAPTIC.LONG]))
+  // await drawSomething();
+
   for await (const data of serial.receive()) {
     console.log('Received data:');
     for (let i = 0; i < data.length; i += 16) {
@@ -110,6 +57,7 @@ async function main() {
       if (line === '0500000800') {
         console.log('pressed 1');
         // await drawSomething();
+        await serial.send(COMMANDS.SET_VIBRATION, Buffer.from([HAPTIC.SHORT]))
       }
     }
   }
--- src/serial.ts
+++ src/serial.ts
@@ -61,9 +61,23 @@ class Serial {
   parser = new MagicByteLengthParser();
   connection: SerialPort;
 
+  protected pendingTransactions: { [key: number]: undefined | ((value: unknown) => void) } = {};
+  protected transactionID = 0;
+  protected getNextTransactionID() {
+    this.transactionID = (this.transactionID + 1) % 256;
+    // Skip transaction ID's of zero since the device seems to ignore them
+    if (this.transactionID === 0) {
+      this.transactionID++;
+    }
+
+    return this.transactionID;
+  }
+
   constructor(path: string) {
     this.connection = new SerialPort({ path, baudRate: 256000 });
   }
+
+  onReceive: (data: Buffer) => void = () => {};
 
   async connect() {
     await new Promise((res) => this.connection.once('open', res));
@@ -80,14 +94,73 @@ class Serial {
       this.connection.write(Buffer.from(WS_UPGRADE_HEADER));
     });
     this.connection.pipe(this.parser);
+    this.parser.on('data', (data) => {
+      const transactionID = data[2];
+      const resolver = this.pendingTransactions[transactionID];
+      if (resolver) {
+        console.log('Transaction resolved');
+        resolver(data);
+        this.pendingTransactions[transactionID] = undefined;
+      } else {
+        this.onReceive(data);
+      }
+    });
+
     console.log('Connected to device');
   }
 
-  // Generator function to receive data instead of this.parser.on('data', (buff) => {})
+  // Make generator using onReceive instead of this.parser.on('data', (buff) => {})
   async *receive() {
-    for await (const data of this.parser) {
-      yield data;
+    while (true) {
+      yield await new Promise((res) => {
+        this.onReceive = res;
+      });
     }
+  } 
+
+  protected getPacketPrefix(packet: Buffer) {
+    // Large messages
+    if (packet.length > 0xff) {
+      const prefix = Buffer.alloc(14);
+      prefix[0] = 0x82;
+      prefix[1] = 0xff;
+      prefix.writeUInt32BE(packet.length, 6);
+      return prefix;
+    }
+    // Small messages
+    // prefixend each message with a send indicating the length to come
+    const prefix = Buffer.alloc(6);
+    prefix[0] = 0x82;
+    prefix[1] = 0x80 + packet.length;
+    return prefix;
+  }
+
+  async send(command: number, data = Buffer.alloc(0)) {
+    const transactionID = this.getNextTransactionID();
+    const header = Buffer.alloc(3);
+    header[0] = Math.min(3 + data.length, 0xff);
+    header[1] = command;
+    header[2] = transactionID;
+    const packet = Buffer.concat([header, data]);
+
+    this.connection.write(this.getPacketPrefix(packet));
+    this.connection.write(packet);
+
+    // // This is not good cause it will break the generator loop
+    // const waitTransaction = (res: (value: any) => void) => {
+    //   this.parser.once('data', (buff) => {
+    //     if (transactionID == buff[2]) {
+    //       console.log('transactionID matches');
+    //       res(void 0);
+    //     } else {
+    //       waitTransaction(res);
+    //     }
+    //   });
+    // };
+    // return new Promise(waitTransaction);
+    return new Promise((res) => {
+      this.pendingTransactions[this.transactionID] = res;
+    });
   }
 }
 
